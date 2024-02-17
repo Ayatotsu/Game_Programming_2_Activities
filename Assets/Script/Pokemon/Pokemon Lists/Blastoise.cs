7b4e45af36f15b07071793e0e3acca0fe18f7c9c
Edgardo Merelos Jr <edgardomerelosjr@gmail.com>
2024-02-17 08:50:44 +0800

    deleted Pokemon Lists game object

--- Assets/Script/Pokemon/Pokemon Lists/Blastoise.cs
+++ Assets/Script/Pokemon/Pokemon Lists/Blastoise.cs
@@ -7,7 +7,16 @@ using TMPro;
 
 public class Blastoise : PokemonData
 {
-
+    [Header("Reference Game Objects")]
+    public TextMeshProUGUI pName;
+    public TextMeshProUGUI pTypes;
+    public TextMeshProUGUI pGender;
+    public TextMeshProUGUI pHp;
+    public TextMeshProUGUI pAtk;
+    public TextMeshProUGUI pSpAtk;
+    public TextMeshProUGUI pDef;
+    public TextMeshProUGUI pSpDef;
+    public TextMeshProUGUI pSpeed;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,5 +31,18 @@ public class Blastoise : PokemonData
         stats.spatk = 85;
         stats.spdef = 105;
         stats.speed = 78;
+    }
+
+    void Update()
+    {
+        pName.text = "Name:  " + pokemonName;
+        pTypes.text = "Types:  " + types;
+        pGender.text = "Gender:  " + gender;
+        pHp.text = "Hp:  " + stats.hp;
+        pAtk.text = "Atk:  " + stats.atk;
+        pSpAtk.text = "SpAtk:  " + stats.spatk;
+        pDef.text = "Def:  " + stats.def;
+        pSpDef.text = "SpDef:  " + stats.spdef;
+        pSpeed.text = "Speed:  " + stats.speed;
     }
 }
